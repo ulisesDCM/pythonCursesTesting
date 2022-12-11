b9d256b7007207b793eeab7617b1c37c5a9fe132
ulisesDCM <ulises.cmorales@hotmail.com>
2022-12-11 00:24:55 -0800

    added two new functions for testing the creation of new windows and colors

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hello world")
+import curses
+from curses import wrapper
+import time
+
+def firstTest(stdscr):
+    stdscr.clear()
+    for i in range(1,11):
+        v = i-0
+        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def secondTest(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE)
+    BLUE_AND_YELLOW = curses.color_pair(1)
+    GREEN_AND_BLACK = curses.color_pair(2)
+    RED_AND_WHITE = curses.color_pair(2)
+
+    newWindow = curses.newwin(1, 20, 10, 10)
+
+    for i in range(100):
+        newWindow.clear()
+
+        color = BLUE_AND_YELLOW
+        if i % 2 == 0:
+            color = GREEN_AND_BLACK
+
+        newWindow.addstr(f"Count {i}",color)
+        newWindow.refresh()
+        time.sleep(0.5)
+    
+    stdscr.getch()
+
+
+if __name__ == '__main__':
+    # wrapper(firstTest)
+    wrapper(secondTest)
