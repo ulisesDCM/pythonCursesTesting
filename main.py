b4aa438aed778655deb6e2a878827f5f7d6c91e2
ulisesDCM <ulises.cmorales@hotmail.com>
2022-12-11 15:31:32 -0800

    added pad example

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import curses
 from curses import wrapper
 import time
 
+# Basic string in the main screen
 def test1(stdscr):
     stdscr.clear()
     for i in range(1,11):
@@ -10,7 +11,7 @@ def test1(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
-
+# Window example 1
 def test2(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -34,18 +35,44 @@ def test2(stdscr):
     
     stdscr.getch()
 
-
+# Window example 2
 def test3(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLUE)
     newWindow = curses.newwin(1, 20, 10, 10)
     newWindow.clear()
     # newWindow.bkgd(curses.color_pair(1))
     newWindow.addstr(f"lines: {curses.LINES-1} " + f"/cols:{curses.COLS-1}", curses.color_pair(1))
+    newWindow.getch()
+    
+    newWindow.mvwin(10,15)
+    # newWindow.refresh()
+    newWindow.getch()
+
+    newWindow.mvwin(10,15)
     newWindow.refresh()
     newWindow.getch()
-    stdscr.getch()
+
+    # stdscr.getch()
+
+# Pad example
+def test4(stdscr):
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
+    pad = curses.newpad(100,100)
+    stdscr.refresh()
+
+    for i in range(100):
+        for j in range(26):
+            char = chr(67+j)
+            pad.addstr(char, curses.color_pair(1))
+    
+    for i in range(50):
+        pad.refresh(0, i, 5, 5, 10, 25)
+        time.sleep(0.2) 
+    stdscr.getch()  #Stop program execution
+
 
 if __name__ == '__main__':
     # wrapper(test1)
     # wrapper(test2)
-    wrapper(test3)
+    # wrapper(test3)
+    wrapper(test4)
